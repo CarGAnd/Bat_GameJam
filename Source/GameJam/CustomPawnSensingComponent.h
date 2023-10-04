6f3dde03c05d85f2a1cd09915798c721f9455bb8
Nenn <47076832+Nenniana@users.noreply.github.com>
2023-10-04 10:07:57 +0200

    Updated Enemy after attempting to text save bug.

--- Source/GameJam/CustomPawnSensingComponent.h
+++ Source/GameJam/CustomPawnSensingComponent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f29b01d8064b15121399f9fc7deb0f59703a077927848d639deb9eed2c41fc2
-size 860
+oid sha256:2f1e1560cd747cd00b060f2723c5ace9d728de224d2981bb21a459f5f55d6918
+size 861
